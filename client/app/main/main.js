14b533deb21155404e5dc016a81946376b375682
Andrew Ivanov <andrey.ivanov@onetwotrip.com>
2016-04-11 01:32:14 +0300

    Boilerplate code

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('uberCodingChallengeApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
