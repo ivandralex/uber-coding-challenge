294c23bd848255a51bf72f62801e9e769684ce93
Andrew Ivanov <andrey.ivanov@onetwotrip.com>
2016-04-13 23:26:22 +0300

    Added model for Food truck

--- server/api/foodtruck/foodtruck.model.js
+++ server/api/foodtruck/foodtruck.model.js
@@ -4,9 +4,29 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var FoodTruckSchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  status: String,
+  expirationdate: Date,
+  permit: String,
+  block: String,
+  received: String,
+  facilitytype: String,
+  blocklot: String,
+  locationdescription: String,
+  cnn: String,
+  priorpermit: String,
+  approved: String,
+  schedule: String,
+  address: String,
+  applicant: String,
+  lot: String,
+  fooditems: String,
+  longitude: String,
+  latitude: String,
+  objectid: String,
+  dayshours: String,
+  x: String,
+  y: String,
+  location: Object
 });
 
 module.exports = mongoose.model('FoodTruck', FoodTruckSchema);
