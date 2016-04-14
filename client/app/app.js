1e1be3ed95600b289cf148d868b5828f728f500d
Andrew Ivanov <andrey.ivanov@onetwotrip.com>
2016-04-14 17:03:19 +0300

    Display google maps

--- client/app/app.js
+++ client/app/app.js
@@ -5,11 +5,17 @@ angular.module('uberCodingChallengeApp', [
   'ngResource',
   'ngSanitize',
   'ui.router',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'uiGmapgoogle-maps'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
-    $urlRouterProvider
-      .otherwise('/');
+    $urlRouterProvider.otherwise('/');
 
     $locationProvider.html5Mode(true);
+  })
+  .config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        key: 'AIzaSyDuqmS410C7CLFP0Vj6mvwb-p-wIKAX2PA',
+        libraries: 'weather,geometry,visualization'
+    });
   });
--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,6 +1,21 @@
 'use strict';
 
 angular.module('uberCodingChallengeApp')
-  .controller('MainCtrl', function ($scope, FoodTruck) {
-    $scope.foodTrucks = FoodTruck.search({longitude: 37.7642669, latitude: -122.4427585});
+  .controller('MainCtrl', function ($scope, FoodTruck, uiGmapGoogleMapApi) {
+    $scope.foodTrucks = FoodTruck.search({
+      latitude: -122.4427585,
+      longitude: 37.7642669
+    });
+
+    $scope.map = {
+      center: {
+        latitude: 37.7832003,
+        longitude: -122.42959
+      },
+      zoom: 14
+    };
+
+    uiGmapGoogleMapApi.then(function(maps) {
+      console.log('maps are ready', maps);
+    });
   });
